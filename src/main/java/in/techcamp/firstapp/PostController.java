7b7ef28a41a6be314c62e31c8f40168f0a9369d6
yuyunaginagi <centrepg8@gmail.com>
2024-04-28 15:06:16 +0900

    一覧表示機能

--- src/main/java/in/techcamp/firstapp/PostController.java
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class PostController {
     @GetMapping("/hello")
@@ -11,5 +13,16 @@ public class PostController {
         var sampleText = "サンプルテキスト";
         model.addAttribute("sampleText", sampleText);
         return "hello";
+    }
+
+    @GetMapping
+    public String showList(Model model) {
+        var postList = List.of(
+                new PostEntity(1, "投稿１"),
+                new PostEntity(2, "投稿２"),
+                new PostEntity(3, "投稿３")
+        );
+        model.addAttribute("postList", postList);
+        return "index";
     }
 }
